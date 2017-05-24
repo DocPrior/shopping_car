57f37ca4eb56bd41033e3555b4d2674c13ed49d2
DocPrior <Sara Prior>
2017-05-24 17:18:08 -0400

    finish after_tax_total, finish shopping cart

--- cart.rb
+++ cart.rb
@@ -30,6 +30,14 @@ class Shopping_carts
     total
   end
 
+  def after_tax_total
+    total = 0
+    @cart.each do |product|
+      total += product.tax
+    end
+    total
+  end
+
 end
 
 cart1 = Shopping_carts.new
@@ -48,3 +56,5 @@ puts cart1.inspect
 cart1.add_to_cart("broom", 15)
 
 puts cart1.before_tax_total
+
+puts cart1.after_tax_total
